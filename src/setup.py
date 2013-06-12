805175ce4da2008aeed8282c6d20bd3ac047a739
Rafael Darder <darder@gmail.com>
2013-06-12 14:22:27 -0300

    include package data both in sdist and bdist

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
+import os
+
+def find_extra_files(path):
+  for dirpath, _, filenames in os.walk(path):
+    if len(filenames) > 0:
+      yield (dirpath, [os.path.join(dirpath, filename)
+                       for filename in filenames])
 
 setup(
   name='rpc',
   version='0.2',
   packages=['rpc'],
   namespace_packages=['rpc'],
-  include_package_data=True,
-  #warning, source eggs (sdist) won't include non python files.
+  data_files=find_extra_files('clients'),
   url='http://github.com/rdarder/rpc',
   install_requires=['gevent', 'gevent-websocket', 'webob' ],
   license='GPL',
